b0248788ec2454f6391610599d69bc38366f6d02
Jani Koivukoski <fts@a91-154-4-224.elisa-laajakaista.fi>
2009-11-22 23:00:58 +0200

    scaffold for instances

--- app/models/exercisegroup.rb
+++ app/models/exercisegroup.rb
@@ -1,4 +1,5 @@
 class Exercisegroup < ActiveRecord::Base
   #named_scope :red, :conditions => {:color => 'red'}
+  belongs_to :instance
   
 end
--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,4 +1,6 @@
 class Instance < ActiveRecord::Base
   
   has_many :exercisegroups
+  belongs_to :course
+  
 end
